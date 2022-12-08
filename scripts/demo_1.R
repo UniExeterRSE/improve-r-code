7f928a64dd84b03fd4352fc90a4c219eb199f69e
aspides-js <73300245+aspides-js@users.noreply.github.com>
2022-12-08 10:28:24 +0000

    include lintr in scripts

--- scripts/demo_1.R
+++ scripts/demo_1.R
@@ -1,3 +1,7 @@
+if (!require("lintr", quietly = TRUE))
+  install.packages("lintr")
+library(lintr)
+
 times=400000
 cols=100
 data<- as.data.frame(matrix(rnorm(times*cols, mean= 5),
@@ -15,3 +19,5 @@ means <- apply(newData[, names(newData)!="id"], 2, mean)
 for (i in 1:length(means)) {
   newData[,names(newData) != "id"][,i] <-newData[,names(newData) != "id"][, i] - means[i]
 }    
+
+lint("demo_1.R")
